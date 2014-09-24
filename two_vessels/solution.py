3a1922e5555e6c2183765df15fb8d9e15c754142
Gammal A. S <gammalas@mars>
2014-09-24 23:28:34 +0400

    * Изменение интерфейсам функции: теперь можно подавать объёмы сосудов в любом порядке. При этом изменился формат возвращаемого результата. Надеюсь, такой формат поможет при печати результатов. * Переформатирование тестов в соответствии с новым форматом возвращаемого значения. * Удаление бессмысленного для нового интерфейса функции теста.

--- two_vessels/solution.py
+++ two_vessels/solution.py
@@ -12,37 +12,39 @@
 # Но в целом решение сводится к анализу категрии состояния в котором сейчас находится решение
 # и принятия однозначного решения как и куда двигаться дальше.
 # Функция get_first_solution() реализует поиск решения для первого случая.
-def get_first_solution(A, B, q, sol = []):
+def get_first_solution(v1, v2, q, sol = []):
         """Поиск решения для первого случая.
 Вход:
-A > 0 - объём первого сосуда;
-B > 0 - объём второго сосуда;
-0 < q <= max(A, B) - объём жидкости, который надо получить;
+v1 > 0 - объём первого сосуда;
+v2 > 0 - объём второго сосуда;
+0 < q <= max(v1, v2) - объём жидкости, который надо получить;
 sol - решение, полученное на предыдущем шаге.
 Выход:
-при A > B: решение вида [...,((x, y), action) ,...],
-где (x, y) - текущие объёмы жидкостей в сосудах,
-action - действие, с помощью которого мы перешли в текущее состояние;
-при A <= B: None."""
-        if A <= B:
-                return None
+Решение вида [...,({A: x, B: y}, action) ,...],
+где x - текущий объёмы жидкости в сосуде объёма A,
+y - текущий объёмы жидкости в сосуде объёма B,
+action - действие, с помощью которого мы перешли в текущее состояние."""
+        (A, B) = (v1, v2) if v1 > v2 else (v2, v1)
         
         if sol == []:
         # есть тольно один вариант анчала решения
-                sol = [((A, 0), acts.fA)]
-        (x, y) = sol[-1][0]
+                sol = [({A: A, B: 0}, acts.fA)]
+        
+        last_step = sol[-1]
+        state = last_step[0]
+        (x, y) = (state[A], state[B])
         if q in (x, y):
         # если текущее состояние содержит требуемый объём жидкости, то заканчиваем
                 return sol
         if y == B:
-                sol += [((x, 0), acts.eB)]
+                sol += [({A: x, B: 0}, acts.eB)]
         if x > B and y == 0:
-                sol += [((x-B, B), acts.A2B),]
+                sol += [({A: x-B, B: B}, acts.A2B),]
         if x < B and y == 0:
-                sol += [((0, x), acts.A2B)]
+                sol += [({A: 0, B: x}, acts.A2B)]
         if x == 0:
-                sol += [((A, y), acts.fA),
-                        ((A - (B-y), B), acts.A2B)]
+                sol += [({A: A, B: y}, acts.fA),
+                        ({A: A - (B-y), B: B}, acts.A2B)]
         return get_first_solution(A, B, q, sol)
 
 
@@ -66,7 +68,7 @@ def test__get_first_solution():
             'B': 3,
             'q': 5,
             },
-        'expected': [((5, 0), acts.fA)]
+        'expected': [({5:5, 3:0}, acts.fA)]
         },
         {
         'input': {
@@ -74,8 +76,8 @@ def test__get_first_solution():
             'B': 3,
             'q': 2,
             },
-        'expected': [((5, 0), acts.fA),
-                     ((2, 3), acts.A2B)]
+        'expected': [({5:5, 3:0}, acts.fA),
+                     ({5:2, 3:3}, acts.A2B)]
         },
         {
         'input': {
@@ -83,22 +85,14 @@ def test__get_first_solution():
             'B': 3,
             'q': 1,
             },
-        'expected': [((5, 0), acts.fA),
-                     ((2, 3), acts.A2B),
-                     ((2, 0), acts.eB),
-                     ((0, 2), acts.A2B),
-                     ((5, 2), acts.fA),
-                     ((4, 3), acts.A2B),
-                     ((4, 0), acts.eB),
-                     ((1, 3), acts.A2B)]
-        },
-        {
-        'input': {
-            'A': 3,
-            'B': 5,
-            'q': 1,
-            },
-        'expected': None
+        'expected': [({5:5, 3:0}, acts.fA),
+                     ({5:2, 3:3}, acts.A2B),
+                     ({5:2, 3:0}, acts.eB),
+                     ({5:0, 3:2}, acts.A2B),
+                     ({5:5, 3:2}, acts.fA),
+                     ({5:4, 3:3}, acts.A2B),
+                     ({5:4, 3:0}, acts.eB),
+                     ({5:1, 3:3}, acts.A2B)]
         },
     )
     test_OK = True
@@ -120,5 +114,3 @@ got:
 
             
 test__get_first_solution()
-
-
