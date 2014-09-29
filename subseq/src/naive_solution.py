5e4e9f2e89733ced0790199f9aafc87e42e1092e
Gammal A. S <gammalas@mars>
2014-09-29 22:07:09 +0400

    * Добавление файла с "наивным" решением. * Добавление примера входного файла.

---
+++ subseq/src/naive_solution.py
@@ -0,0 +1,54 @@
+# -*- coding: cp1251 -*-
+
+import itertools
+
+
+def fetch_head(seq, n):
+    return list(itertools.islice(seq, 0, n))
+
+
+class RingBuffer:
+    def __init__(self, buf_size, data_seq):
+        self.buf_size = buf_size
+        self.data = fetch_head(data_seq, buf_size)
+        
+    def append(self, x):
+        self.data.pop(0)
+        self.data.append(x)
+
+
+def find_subseq(subseq, seq):
+    sublist = list(subseq)
+    seq_iter = iter(seq)
+    buf = RingBuffer(len(sublist), seq_iter)
+    i = 0
+    while buf.data != sublist:
+        buf.append(seq_iter.next())
+        i += 1
+    return i
+
+
+def gen_num_seq():
+    # Генератор элементов бесконечной последовательности
+    i = 1
+    while 1:
+        s = str(i)
+        for ch in s:
+            yield ch
+        i += 1
+
+
+def find_solution_naively(subseq):
+    return find_subseq(subseq, gen_num_seq()) + 1
+
+
+def main():
+    fin = open('in.txt')
+    fout = open('out.txt', 'w')
+    for line in fin:
+        subseq = line.strip()
+        fout.write('%d\n' % find_solution_naively(subseq))
+
+
+if __name__ == '__main__':
+    main()
